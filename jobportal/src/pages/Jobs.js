681731890187a1e33400ef6cbbaed7eb1d00d620
dsd739 <dsd739@gmail.com>
2023-09-13 13:59:33 +0530

    Changes to be committed:

--- jobportal/src/pages/Jobs.js
+++ jobportal/src/pages/Jobs.js
@@ -1,44 +1,47 @@
-import React, { useEffect } from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import { Container, Form, FormControl } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllJobs, reset } from "../store/reducers/JobSlice";
+import React, { useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Container, Form, FormControl } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllJobs, reset } from '../store/reducers/jobSlice';
 
 function Appliedjob() {
-  const jobs = useSelector((state) => state?.job?.jobs);
+  const jobs = useSelector(state => state?.job?.jobs);
   const dispatch = useDispatch();
 
-  useEffect(() => {
+  useEffect(()=> {
     dispatch(getAllJobs());
-  }, []);
+  },[])
 
-  useEffect(() => {
-    return () => {
-      dispatch(reset());
-    };
-  });
+  useEffect(()=> {
+    return ()=> {
+      dispatch(reset())
+    }
+  })
 
   const renderJobs = () => {
-    return jobs.map((job) => {
-      const { title, maxSalary, description } = job;
+    return jobs.map(job => {
+      const {title, maxSalary, description} = job;
       return (
         <div className="d-flex justify-content-center">
-          <Card>
-            <Card.Header className="d-flex justify-content-between align-items-center">
-              <span>{title}</span>
-              <span>MAX Salary {maxSalary}</span>
-            </Card.Header>
-            <Card.Body>
-              <Card.Title>Description</Card.Title>
-              <Card.Text>{description}</Card.Text>
-              <Button variant="primary">Apply</Button>
-            </Card.Body>
-          </Card>
-        </div>
-      );
-    });
-  };
+        <Card>
+          <Card.Header className="d-flex justify-content-between align-items-center">
+            <span>{title}</span>
+            <span>MAX Salary {maxSalary}</span>
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>Description</Card.Title>
+            <Card.Text>
+            {description}
+      
+            </Card.Text>
+            <Button variant="primary">Apply</Button>
+          </Card.Body>
+        </Card>
+      </div>
+      )
+    })
+  }
   return (
     <div className="wrapper">
       <Container className="vh-100">
@@ -46,8 +49,9 @@ function Appliedjob() {
         <Form inline className="justify-content-center mb-3">
           <FormControl type="search" placeholder="Search" />
         </Form>
-        {renderJobs()}
+        {renderJobs()}        
       </Container>
+    
     </div>
   );
 }
--- jobportal/src/store/index.js
+++ jobportal/src/store/index.js
@@ -1,12 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import jobSlice from "./reducers/JobSlice";
-import userSlice from "./reducers/userSlice";
-import notificationSlice from "./reducers/notificationSlice";
+import {configureStore} from '@reduxjs/toolkit';
+import jobSlice from './reducers/jobSlice';
+import userSlice from './reducers/userSlice';
+import notificationSlice from './reducers/notificationSlice';
 
 export const store = configureStore({
-  reducer: {
-    job: jobSlice,
-    user: userSlice,
-    notification: notificationSlice,
-  },
-});
+    reducer: {
+        job: jobSlice,
+        user:userSlice,
+        notification: notificationSlice        
+    }
+})
